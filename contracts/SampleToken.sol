79b1b353d79d45791c0827fda88b944085a88c74
Jemil <oyebisijemil@gmail.com>
2021-07-24 00:07:47 -0400

    Added delegated transfer functionality A situation where the owner of the account approve the spender to transfer token on his/her behalf This completes the ERC20 token implementation

--- contracts/SampleToken.sol
+++ contracts/SampleToken.sol
@@ -6,15 +6,18 @@ contract SampleToken {
   string public name = "SampleToken";
   string public symbol = "SMLT";
   string public standard = "Sample Token v1.0";
-
-  event Transfer(address indexed from, address indexed to, uint256 value);
-
-  // set the total supply of tokens to 1,000,000
   uint public totalSupply;
 
   //key is the address of the account that has the token balance
   //uint256 is the amount of tokens owned by the address
   mapping (address => uint256) public balanceOf;
+  //this keeps track of an account owner to keep track of its approval
+  //AccountA approving accountB with valueC
+  mapping(address => mapping(address => uint256)) public allowance;
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+
 
   constructor(uint256 _initialSupply) public {
     totalSupply = _initialSupply;
@@ -23,7 +26,8 @@ contract SampleToken {
     balanceOf[msg.sender] = _initialSupply;
   }
 
-  //function to transfer tokens to another account
+  //function to transfer tokens to another account, by the owner of the account
+  //Making the call by yourself
   function transfer(address _to, uint256 _value) public returns(bool success){
     require(balanceOf[msg.sender] >= _value);
     balanceOf[_to] += _value;
@@ -33,4 +37,35 @@ contract SampleToken {
 
     return true;
   }
+
+  //Delegated functions to transfer tokens to another account, by the owner of the account
+  //function for exchange to transfer token to another account on your behalf
+  //The transferFrom allow us to handle the delegated transfer
+  //The transferFrom acts like transfer, but on behalf of the account that has the token balance/ on my behalf
+  function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
+    //require _from has enough tokens to transfer
+    require(balanceOf[_from] >= _value);
+    //require allowance is big enough to transfer, that is to check if the amount is allowed
+    require(allowance[_from][msg.sender] >= _value);
+    //move money arround by changing the balance
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+    //update the allowance
+    allowance[_from][msg.sender] -= _value;
+    emit Transfer(_from, _to, _value);
+    //emit event
+    return true;
+  }
+
+  //The amount the spender is allowed to withdraw from the owner
+
+  //function to approve exchange to transfer tokens to another account on your behalf
+  //msg.sender is the owner of the token which is you
+  //approve and store the amount approved in the allowance state variable
+  function approve(address _spender, uint256 _value) public returns (bool success){
+    allowance[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
 }
--- test/sample_token.js
+++ test/sample_token.js
@@ -51,6 +51,69 @@ contract("SampleToken", function ( accounts ) {
      }).then(function(success){
        assert.equal(success, true);
      });
-
   });
+
+  it("should approve token for delegated transfer", async function(){
+    let sampleToken;
+    return SampleToken.deployed().then(function(instance){
+      sampleToken = instance;
+      return sampleToken.approve.call(accounts[1], 100, {from: accounts[0]});
+    }).then(function(success){
+      assert.equal(success, true);
+      return sampleToken.approve(accounts[1], 100, {from: accounts[0]});
+    }).then(function(receipt){
+      assert.equal(receipt.logs.length, 1);
+      assert.equal(receipt.logs[0].event, "Approval");
+      assert.equal(receipt.logs[0].args.owner, accounts[0]);
+      assert.equal(receipt.logs[0].args.spender, accounts[1]);
+      assert.equal(receipt.logs[0].args.value.toNumber(), 100);
+
+      return sampleToken.allowance(accounts[0], accounts[1]);//did account[0] approve account[1] 100 tokens?
+    }).then(function(allowance){
+      assert.equal(allowance.toNumber(), 100);
+    });
+  })
+
+  it("should handle delegated token transfer", async function(){
+    return SampleToken.deployed().then(function(instance){
+      sampleToken = instance;
+      fromAccount = accounts[2];
+      toAccount = accounts[3];
+      spendingAccount = accounts[4];
+
+      //Transfer some tokens to fromAccount
+      return sampleToken.transfer(fromAccount, 100, {from: accounts[0]});
+    }).then(function(receipt){
+      //Approve spendingAccount to spend 10 tokens from fromAccount
+      return sampleToken.approve(spendingAccount, 10, {from: fromAccount});
+    }).then(function(receipt){
+      //Try to transfer larger amount than the sender's balance
+      return sampleToken.transferFrom(fromAccount, toAccount, 1000, {from: spendingAccount});
+    }).then(assert.fail).catch(function(error){
+      assert(error.message.indexOf("revert") >= 0);
+      //Try to transfer less than the sender's balance
+      return sampleToken.transferFrom(fromAccount, toAccount, 11, {from: spendingAccount});
+    }).then(assert.fail).catch(function(error){
+      assert(error.message.indexOf("revert") >= 0);
+      return sampleToken.transferFrom.call(fromAccount, toAccount, 5, {from: spendingAccount});
+    }).then(function(success){
+      assert.isTrue(success);
+      return sampleToken.transferFrom(fromAccount, toAccount, 5, {from: spendingAccount});
+    }).then(function(receipt){
+      assert.equal(receipt.logs.length, 1);
+      assert.equal(receipt.logs[0].event, "Transfer");
+      assert.equal(receipt.logs[0].args.from, fromAccount);
+      assert.equal(receipt.logs[0].args.to, toAccount);
+      assert.equal(receipt.logs[0].args.value.toNumber(), 5);
+      return sampleToken.balanceOf(fromAccount);
+    }).then(function(balance){
+      assert.equal(balance.toNumber(), 95);
+      return sampleToken.balanceOf(toAccount);
+    }).then(function(balance){
+      assert.equal(balance.toNumber(), 5);
+      return sampleToken.allowance(fromAccount, toAccount);
+    }).then(function(allowance){
+      assert.equal(allowance.toNumber(), 0);
+    })
+  })
 });
