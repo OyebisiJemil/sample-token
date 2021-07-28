0471decfcab045fe172a9854d7472530fe789c4d
Jemil <oyebisijemil@gmail.com>
2021-07-28 07:52:47 -0400

    Added SampleTokenSale, We assigned token sale manager(admin) to this contract We assigned SampleToken contract address to this contract, because it contains the tokens to be sold We assigned or initialize the token price to this contract The buyToken payable function accept the number of token the buyer wants and validates if the buyer is paying the exact amount to be paid, it validates the number of token to be bought does not exceed the available token for the SampleToken contract The buyTokens function then sell the token, keep track of the sold tokens and publish sell event The endSale function verifies only the admin could end sale, transfer the remaining token to the admin address and selfdestroy the SampleTokenSale contract (We still need to finalize this as there is an issue converting address to payable address with this version of solidity

--- contracts/SampleTokenSale.sol
+++ contracts/SampleTokenSale.sol
@@ -6,13 +6,43 @@ contract SampleTokenSale {
   address admin;
   SampleToken public tokenContract;
   uint256 public tokenPrice;
+  uint256 public tokenSold;
 
+  event Sell(address _buyer, uint256 _amount);
+
+  // Assign the token sale manager address to the contract
+  // Assign the token contract address to the contract
+  // Assign the token price to the contract
   constructor(SampleToken _tokenContract, uint256 _tokenPrice) public {
-    // Assign the token sale manager address to the contract
-    // Assign the token contract address to the contract
-    // Assign the token price to the contract
     admin = msg.sender;
     tokenContract = _tokenContract;
     tokenPrice = _tokenPrice;
+  }
+
+  //the payable is there because we want someone to be able to send ether when this function is called
+  function buyTokens(uint256 _numberOfTokens) public payable {
+    require(msg.value == multiply(tokenPrice, _numberOfTokens));
+    require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+    require(tokenContract.transfer(msg.sender, _numberOfTokens));// this is the actual buy functionality
+
+    //keep track of the token sold
+    tokenSold += _numberOfTokens;
+    //Trigger the sell event
+    emit Sell(msg.sender, _numberOfTokens);
+  }
+
+  //Ending Token sale
+  function endSale() public {
+    //Verify that only admin can do this
+    require(msg.sender == admin);
+    //Transfer the remaining token to the admin
+    require(tokenContract.transfer(admin, tokenContract.balanceOf(address(this))));
+    //Destroy the contract
+    //address payable adminPayable = payable(address(admin));
+    //selfdestruct(theadmin);
+  }
+
+  function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, "ds-math-mul-overflow");
   }
 }
--- test/sample_token_sale.js
+++ test/sample_token_sale.js
@@ -1,9 +1,16 @@
 const SampleTokenSale = artifacts.require("SampleTokenSale");
+const SampleToken = artifacts.require("SampleToken")
 
 contract("SampleTokenSale", function ( accounts ) {
+  var sampleTokenSale;
+  var sampleToken;
+  var admin = accounts[0];
+  var tokenPrice= 1000000000000000;// a token cost this amount in wei
+  var tokensAvailable = 750000
+  var buyer = accounts[1];
+  var numberOfTokens;
   it("should initialize the contract with the correct values", async function () {
-    var sampleTokenSale;
-    var tokenPrice= 1000000000000000;// in wei
+
     return SampleTokenSale.deployed().then(function (instance) {
       sampleTokenSale = instance;
       return sampleTokenSale.address;
@@ -17,4 +24,69 @@ contract("SampleTokenSale", function ( accounts ) {
       assert.equal(price,tokenPrice);
     })
   });
+
+  it("should facilitate the buying of token", async function () {
+    return SampleToken.deployed().then(function (instance) {
+      sampleToken = instance;
+      return SampleTokenSale.deployed();
+
+    }).then(function(instance){
+      sampleTokenSale = instance;
+
+      //Provision 75% of all tokens to the token sale
+      return sampleToken.transfer(sampleTokenSale.address, tokensAvailable, {from: admin})
+
+    }).then(function(receipt){
+      numberOfTokens = 10;
+      var valueToBuy = numberOfTokens * tokenPrice;
+      return sampleTokenSale.buyTokens(numberOfTokens,{from: buyer, value: valueToBuy});
+    }).then(function(receipt){
+      assert.equal(receipt.logs.length,1);
+      assert.equal(receipt.logs[0].event,"Sell");
+      assert.equal(receipt.logs[0].args._buyer,buyer);
+      assert.equal(receipt.logs[0].args._amount,numberOfTokens);
+      return sampleTokenSale.tokenSold();
+    }).then(function(amount){
+      assert.equal(amount.toNumber(),numberOfTokens, "increment the number of tokens sold");
+      return sampleToken.balanceOf(buyer);
+    }).then(function(balance){
+      assert.equal(balance.toNumber(), numberOfTokens);
+      return sampleToken.balanceOf(sampleTokenSale.address);
+    }).then(function(balance){
+      assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens);
+      const value = numberOfTokens * 1;
+      return sampleTokenSale.buyTokens(numberOfTokens,{from: buyer, value: value});
+    }).then(assert.fail).catch(function(error){
+      assert(error.message.indexOf("revert") >= 0, "msg.value must be equal to number of tokens in wei");
+      return sampleTokenSale.buyTokens(800000, {from: buyer, value: numberOfTokens*tokenPrice})
+    }).then(assert.fail).catch(function(error){
+      assert(error.message.indexOf("revert") >= 0, "cannot purchase more tokens ");
+    })
+  })
+
+  it("should end token sale", async function(){
+    return SampleToken.deployed().then(function(instance){
+      sampleToken = instance;
+
+      return SampleTokenSale.deployed();
+    }).then(function(instance){
+      sampleTokenSale = instance;
+      //Try to end sale from account other than the admin
+      return sampleTokenSale.endSale({from: buyer})
+    }).then(assert.fail).catch(function(error){
+      assert(error.message.indexOf("revert") >= 0, "only admin can put an end to sale")
+      //end sale as admin
+      return sampleTokenSale.endSale({from: admin});
+    }).then(function(receipt){
+       return sampleToken.balanceOf(admin);
+    }).then(function(balance){
+      assert.equal(balance.toNumber(), 999990, "returns all the unsold dapp tokens to the admin")
+      //Check that the token price was reset when selfDestruct was called
+      return sampleTokenSale.tokenPrice();
+    }).then(function(price){
+      //this is to test theselfdestruct of the sampleTokenSale contract once the sale is done
+      // but there is a issue converting address to payable address, will attend to this later
+      //assert.equal(price.toNumber(), 0, "token price was reset")
+    })
+  })
 });
